cd6826e38422e34cf121cc78f2fabfd3b94d6c4b
Metehan Özden <metehanozden.bil@gmail.com>
2024-10-21 15:53:04 +0300

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Metehan Özden on 21.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
